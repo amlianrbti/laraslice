d3d8eb778382a2f7cd50554b7afbc682a0c62281
amlianrbti <nurbaetiamelia204@gmail.com>
2024-03-02 09:48:51 +0700

    baru

---
+++ resources/views/layouts/admin/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2024-{{ date('Y') }} <div class="bullet"></div> webpro
+    </div>
+    <div class="footer-right">
+        0.0.3
+    </div>
+</footer>
